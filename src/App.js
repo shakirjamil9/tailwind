e1583df7e97245636feeb0538d9e0eb9166dae60
shakirjamil9 <64136350+shakirjamil9@users.noreply.github.com>
2021-04-22 23:05:43 +0500

    tailwind first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import dp from "./images/dp.JPG";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto bg-blue-700 py-5">
+      <div className="flex flex-wrap justify-evenly items-start h-96 border border-green-900">
+        <div className="m-2 h-48 w-48 bg-green-500"></div>
+        <div className="m-2 h-48 w-48 bg-red-500"></div>
+        <div className="m-2 h-48 w-48 bg-purple-500"></div>
+      </div>
+      {/* Custom Card */}
+      <div className="max-w-sm flex p-4 animate-bounce bg-white rounded-lg mx-auto shadow-2xl">
+        <div className="flex-shrink-0">
+          <img src={dp} className="h-12 w-12 rounded-full" />
+        </div>
+        <div className="ml-6 pt-1">
+          <h4 className="text-xl text-gray-900">Shakir</h4>
+          <p className="text-base text-gray-600">You've a new message</p>
+        </div>
+      </div>
+      <div className="border border-black max-h-20 overflow-y-scroll">
+        <div>
+          <div className="sticky top-0 p-5 bg-green-300 tracking-normal">Sticky Heading 1</div>
+          <p className="py-4">Hello1</p>
+        </div>
+        <div>
+          <div className="sticky top-0 p-5 bg-green-300">Sticky Heading 2</div>
+          <p>Hello1</p>
+        </div>
+        <div>
+          <div className="sticky top-0 ">Sticky Heading 3</div>
+          <p>Hello1</p>
+        </div>
+      </div>
     </div>
   );
 }
